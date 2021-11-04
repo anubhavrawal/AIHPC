67070b6b3363463ae7ad06dbb72a11caee4340f2
Anubhav Rawal <rawal.anubhav@outlook.com>
2021-11-03 23:06:43 -0400

    updated comments

--- read.py
+++ read.py
@@ -14,6 +14,7 @@ import numpy as np
 model = load_model("test.h5")
 
 #Resizze to 28px28p
+#Use painteditor with 3-5pixel tickness
 img = Image.open('/mnt/c/Users/Anubhav_R/Desktop/tmp/AIHPC/3.png').convert("L")
 
 plt.subplot(1, 2, 1)
--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+#Credit: https://docs.databricks.com/_static/notebooks/deep-learning/mnist-tensorflow-keras.html
 import tensorflow as tf
 from tensorflow.keras.datasets import mnist
 from tensorflow.keras.models import Sequential
@@ -13,28 +14,13 @@ import numpy as np
 
 img_rows, img_cols = 28, 28
 
+
 # Specify training parameters
 batch_size = 128
 epochs = 5
 num_classes = 10
 verbose = 1
-'''
-def get_dataset(num_classes, rank=0, size=1):
-    (x_train, y_train), (x_test, y_test) = mnist.load_data('MNIST-data-%d' % rank)
-    x_train = x_train[rank::size]
-    y_train = y_train[rank::size]
-    x_test = x_test[rank::size]
-    y_test = y_test[rank::size]
-    x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
-    x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
-    x_train = x_train.astype('float32')
-    x_test = x_test.astype('float32')
-    x_train /= 255
-    x_test /= 255
-    y_train = utils.to_categorical(y_train, num_classes)
-    y_test = utils.to_categorical(y_test, num_classes)
-    return (x_train, y_train), (x_test, y_test)
-'''
+
 def get_dataset(num_classes, rank=0, size=1):
     (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
